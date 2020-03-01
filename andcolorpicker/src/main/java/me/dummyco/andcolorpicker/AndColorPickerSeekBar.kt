d935bae9af384118b6f9a3f41f934440b4983a0b
DummyCo <ilyaachour@gmail.com>
2020-03-01 23:33:25 +0200

    Implement thumb proper sizing/ripple

--- andcolorpicker/src/main/java/me/dummyco/andcolorpicker/AndColorPickerSeekBar.kt
+++ andcolorpicker/src/main/java/me/dummyco/andcolorpicker/AndColorPickerSeekBar.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.drawable.AnimatedStateListDrawable
-import android.graphics.drawable.AnimationDrawable
-import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.*
+import android.os.Build
 import android.util.AttributeSet
 import android.util.StateSet
 import android.widget.SeekBar
@@ -15,7 +14,7 @@ import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.core.graphics.ColorUtils
 
 class AndColorPickerSeekBar : AppCompatSeekBar,
-                              OnSeekBarChangeListener {
+    OnSeekBarChangeListener {
 
   companion object {
 
@@ -47,12 +46,28 @@ class AndColorPickerSeekBar : AppCompatSeekBar,
     init()
   }
 
-  lateinit var thumbInnerDrawable: GradientDrawable
+  lateinit var thumbDrawableDefaultWrapper: LayerDrawable
+  lateinit var thumbDrawablePressed: GradientDrawable
+  private val coloringDrawables = hashSetOf<Drawable>()
 
   private fun init() {
     setOnSeekBarChangeListener(this)
 
     splitTrack = false
+
+    background = background
+        .mutate()
+        .also {
+          if (it is RippleDrawable) {
+            // TODO: Set ripple size for pre-M too
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              val rippleSizePx = resources
+                  .getDimensionPixelOffset(R.dimen.acp_thumb_ripple_radius)
+              it
+                  .radius = rippleSizePx
+            }
+          }
+        }
 
     progressDrawable = GradientDrawable(
         GradientDrawable.Orientation.LEFT_RIGHT,
@@ -65,97 +80,169 @@ class AndColorPickerSeekBar : AppCompatSeekBar,
             Color.MAGENTA,
             Color.RED
         )
-    ).also {
-      it.setStroke(
-          30,
-          Color.TRANSPARENT
-      )
-      it.cornerRadius = resources.getDimensionPixelOffset(R.dimen.acp_seek_background_corner_radius)
-          .toFloat()
-      it.shape = GradientDrawable.RECTANGLE
-    }
+    )
+        .also {
+          it
+              .setStroke(
+                  resources
+                      .getDimensionPixelOffset(R.dimen.acp_seek_background_padding),
+                  Color.TRANSPARENT
+              )
+          it
+              .cornerRadius = resources
+              .getDimensionPixelOffset(R.dimen.acp_seek_background_corner_radius)
+              .toFloat()
+          it
+              .shape = GradientDrawable
+              .RECTANGLE
+        }
 
-    val thumbSizePx = resources.getDimensionPixelOffset(R.dimen.acp_thumb_size)
+    val thumbFullSizePx = resources
+        .getDimensionPixelOffset(R.dimen.acp_thumb_size_full)
+    val thumbDefaultSizePx = resources
+        .getDimensionPixelOffset(R.dimen.acp_thumb_size_default)
 
-    thumbInnerDrawable = GradientDrawable().also {
-      it.color = ColorStateList.valueOf(Color.WHITE)
-      it.shape = GradientDrawable.OVAL
-      it.setSize(
-          thumbSizePx,
-          thumbSizePx
-      )
-    }
+    val sizeD = thumbFullSizePx - thumbDefaultSizePx
+    val sizeDHalf = sizeD / 2
 
-    // TODO: Try to use ScaleDrawable
-    thumb = AnimatedStateListDrawable().also { animatedStateListDrawable ->
-      animatedStateListDrawable.addState(
-          intArrayOf(android.R.attr.state_pressed),
-          GradientDrawable().also {
-            it.color = ColorStateList.valueOf(Color.BLUE)
-            it.shape = GradientDrawable.OVAL
-            it.setSize(
-                thumbSizePx * 2,
-                thumbSizePx * 2
-            )
-          },
-          1
-      )
-      animatedStateListDrawable.addState(
-          StateSet.WILD_CARD,
-          thumbInnerDrawable,
-          0
-      )
-      //animatedStateListDrawable.addTransition(
-      //    0,
-      //    1,
-      //    AnimationDrawable().also {
-      //      it.addFrame(
-      //          GradientDrawable().also {
-      //            it.setSize(
-      //                160,
-      //                160
-      //            )
-      //            it.color = ColorStateList.valueOf(Color.BLACK)
-      //          },
-      //          1500
-      //      )
-      //      it.addFrame(
-      //          GradientDrawable().also {
-      //            it.setSize(
-      //                160,
-      //                160
-      //            )
-      //            it.color = ColorStateList.valueOf(Color.BLUE)
-      //          },
-      //          1500
-      //      )
-      //    },
-      //    true
-      //)
-    }
+    thumbDrawableDefaultWrapper = LayerDrawable(
+        arrayOf(
+            GradientDrawable().also {
+              it
+                  .color = ColorStateList
+                  .valueOf(Color.TRANSPARENT)
+              it
+                  .shape = GradientDrawable
+                  .OVAL
+              it
+                  .setSize(
+                      thumbFullSizePx,
+                      thumbFullSizePx
+                  )
+            },
+            GradientDrawable().also {
+              it
+                  .color = ColorStateList
+                  .valueOf(Color.WHITE)
+              it
+                  .shape = GradientDrawable
+                  .OVAL
+              it
+                  .setSize(
+                      thumbDefaultSizePx,
+                      thumbDefaultSizePx
+                  )
+            }
+        )
+    )
+        .also {
+          it
+              .setLayerInset(1, sizeDHalf, sizeDHalf, sizeDHalf, sizeDHalf)
+        }
 
-    thumbOffset -= 20
+    thumbDrawablePressed = GradientDrawable()
+        .also {
+          it
+              .color = ColorStateList
+              .valueOf(Color.WHITE)
+          it
+              .shape = GradientDrawable
+              .OVAL
+          it
+              .setSize(
+                  thumbFullSizePx,
+                  thumbFullSizePx
+              )
+        }
+
+    coloringDrawables
+        .add(thumbDrawableDefaultWrapper)
+    coloringDrawables
+        .add(thumbDrawablePressed)
+
+    thumb = AnimatedStateListDrawable()
+        .also { animatedStateListDrawable ->
+          animatedStateListDrawable
+              .addState(
+                  intArrayOf(android.R.attr.state_pressed),
+                  thumbDrawablePressed,
+                  1
+              )
+          animatedStateListDrawable
+              .addState(
+                  StateSet.WILD_CARD,
+                  thumbDrawableDefaultWrapper,
+                  0
+              )
+          //animatedStateListDrawable.addTransition(
+          //    0,
+          //    1,
+          //    AnimationDrawable().also {
+          //      it.addFrame(
+          //          GradientDrawable().also {
+          //            it.setSize(
+          //                160,
+          //                160
+          //            )
+          //            it.color = ColorStateList.valueOf(Color.BLACK)
+          //          },
+          //          1500
+          //      )
+          //      it.addFrame(
+          //          GradientDrawable().also {
+          //            it.setSize(
+          //                160,
+          //                160
+          //            )
+          //            it.color = ColorStateList.valueOf(Color.BLUE)
+          //          },
+          //          1500
+          //      )
+          //    },
+          //    true
+          //)
+        }
+
+    //thumbOffset -= 20
 
     refreshThumb()
   }
 
   private fun refreshThumb() {
-    val thumbStrokeWidthPx = resources.getDimensionPixelOffset(R.dimen.acp_thumb_stroke_width)
 
-    thumbInnerDrawable.setStroke(
-        thumbStrokeWidthPx,
-        ColorUtils.HSLToColor(
-            floatArrayOf(
-                progress.toFloat(),
-                1f,
-                0.5f
+    coloringDrawables
+        .forEach {
+          when (it) {
+            is GradientDrawable -> {
+              paintDrawableStroke(it)
+            }
+            is LayerDrawable -> {
+              paintDrawableStroke(it.getDrawable(1) as GradientDrawable)
+            }
+          }
+        }
+  }
+
+  private fun paintDrawableStroke(drawable: GradientDrawable) {
+    val thumbStrokeWidthPx = resources
+        .getDimensionPixelOffset(R.dimen.acp_thumb_stroke_width)
+
+    drawable
+        .setStroke(
+            thumbStrokeWidthPx,
+            ColorUtils.HSLToColor(
+                floatArrayOf(
+                    progress.toFloat(),
+                    1f,
+                    0.5f
+                )
             )
         )
-    )
   }
 
   override fun onDraw(canvas: Canvas) {
-    super.onDraw(canvas)
+    super
+        .onDraw(canvas)
   }
 
   override fun onProgressChanged(
