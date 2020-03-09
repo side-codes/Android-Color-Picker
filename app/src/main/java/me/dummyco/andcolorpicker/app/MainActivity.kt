d74e97e9f23af3fb34f64aa0ebc4b8110864d5df
DummyCo <ilyaachour@gmail.com>
2020-03-09 19:22:10 +0200

    Enhance drawer menu

--- app/src/main/java/me/dummyco/andcolorpicker/app/MainActivity.kt
+++ app/src/main/java/me/dummyco/andcolorpicker/app/MainActivity.kt
@@ -3,14 +3,19 @@ package me.dummyco.andcolorpicker.app
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.mikepenz.iconics.IconicsColor
 import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
+import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
 import com.mikepenz.iconics.typeface.library.materialdesigndx.MaterialDesignDx
+import com.mikepenz.iconics.utils.color
 import com.mikepenz.iconics.utils.icon
 import com.mikepenz.materialdrawer.holder.ImageHolder
 import com.mikepenz.materialdrawer.holder.StringHolder
@@ -51,9 +56,11 @@ class MainActivity : AppCompatActivity(), ColorizationConsumer {
             it.identifier = page.hashCode().toLong()
             it.name = StringHolder(page.title)
             it.icon = ImageHolder(
-              IconicsDrawable(this@MainActivity).icon(page.icon)
+              IconicsDrawable(this@MainActivity)
+                .icon(page.icon)
+                .color { IconicsColor.colorRes(R.color.colorPrimary) }
             )
-            it.isSelectable = page.fragmentCreator != null
+            it.isSelectable = page.fragmentProducer != null
           }
         }
       )
@@ -74,6 +81,14 @@ class MainActivity : AppCompatActivity(), ColorizationConsumer {
 
   private fun navigateTo(page: Page) {
     when (page) {
+      Page.GITHUB -> {
+        startActivity(
+          Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://github.com/DummyCo/andColorPicker")
+          )
+        )
+      }
       Page.OSS -> {
         startActivity(
           Intent(
@@ -83,7 +98,7 @@ class MainActivity : AppCompatActivity(), ColorizationConsumer {
         )
       }
       else -> {
-        page.fragmentCreator?.let {
+        page.fragmentProducer?.let {
           supportFragmentManager
             .beginTransaction()
             .replace(
@@ -146,42 +161,46 @@ class MainActivity : AppCompatActivity(), ColorizationConsumer {
 
   enum class Page(
     val title: String,
-    val icon: MaterialDesignDx.Icon,
-    val fragmentCreator: (() -> Fragment)? = null
+    val icon: IIcon,
+    val fragmentProducer: (() -> Fragment)? = null
   ) {
     HLS_SEEK_BAR(
       "HSL SeekBar",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_space_bar,
       { HslSeekBarFragment() }
     ),
     HLS_PLANE(
       "HSL Plane",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_fullscreen,
       { WipFragment() }
     ),
     RGB_SEEK_BAR(
       "RGB SeekBar",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_space_bar,
       { WipFragment() }
     ),
     RGB_PLANE(
       "RGB Plane",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_fullscreen,
       { WipFragment() }
     ),
     RGB_CIRCLE(
       "RGB Circle",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_lens,
       { WipFragment() }
     ),
     SWATCHES(
       "Swatches",
-      MaterialDesignDx.Icon.gmf_color_lens,
+      MaterialDesignDx.Icon.gmf_view_comfy,
       { WipFragment() }
+    ),
+    GITHUB(
+      "GitHub",
+      FontAwesome.Icon.faw_github
     ),
     OSS(
       "Open Source Notices",
-      MaterialDesignDx.Icon.gmf_color_lens
+      MaterialDesignDx.Icon.gmf_emoji_people
     )
   }
 }
