bf1db8b01f92cfb86084284f17fdcdac8a4a0580
DummyCo <ilyaachour@gmail.com>
2020-03-24 22:27:04 +0200

    Add CMYK GitHub sample

---
+++ app/src/main/java/codes/side/andcolorpicker/app/fragment/CMYKSeekBarGithubSampleFragment.kt
@@ -0,0 +1,6 @@
+package codes.side.andcolorpicker.app.fragment
+
+import androidx.fragment.app.Fragment
+import codes.side.andcolorpicker.app.R
+
+class CMYKSeekBarGithubSampleFragment : Fragment(R.layout.fragment_cmyk_seek_bar_github_sample)
