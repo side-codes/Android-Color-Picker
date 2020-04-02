317ab15515be8761c62e3041108547f8ec19d5f8
Illia Achour <ilyaachour@gmail.com>
2020-04-02 23:01:44 +0300

    Implement ColorPickerSeekBarSet & RGBColorPickerSeekBarSet

---
+++ andcolorpicker/src/main/java/codes/side/andcolorpicker/view/set/ColorPickerSeekBarSet.kt
@@ -0,0 +1,63 @@
+package codes.side.andcolorpicker.view.set
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.StringRes
+import codes.side.andcolorpicker.R
+import codes.side.andcolorpicker.group.PickerGroup
+import codes.side.andcolorpicker.model.Color
+
+abstract class ColorPickerSeekBarSet<C : Color> @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyle: Int = 0
+) : LinearLayout(
+  context,
+  attrs,
+  defStyle
+) {
+  companion object {
+    private const val TAG = "ColorPickerSeekBarSet"
+    internal const val defaultTextAppearance = android.R.style.TextAppearance_Material_Caption
+  }
+
+  val pickerGroup = PickerGroup<C>()
+
+  init {
+    orientation = VERTICAL
+    init(attrs)
+  }
+
+  private fun init(attrs: AttributeSet? = null) {
+    val typedArray = context.theme.obtainStyledAttributes(
+      attrs,
+      R.styleable.ColorPickerSeekBarSet,
+      0,
+      0
+    )
+
+    typedArray.recycle()
+  }
+
+  protected fun addLabel(@StringRes titleResId: Int) {
+    addView(
+      TextView(
+        context
+      ).also {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+          @Suppress("DEPRECATION")
+          it.setTextAppearance(
+            context,
+            defaultTextAppearance
+          )
+        } else {
+          it.setTextAppearance(defaultTextAppearance)
+        }
+        it.setText(titleResId)
+      }
+    )
+  }
+}
